e2f9c4d0e20691407f15b4cea669aa5f116c9eb1
Matt Javier <matthewrjavier@gmail.com>
2020-08-24 11:41:51 -0700

    initial inquirer prompt

--- public/app.js
+++ public/app.js
@@ -1,1 +1,34 @@
 const { prompt } = require('inquirer')
+
+const start () => {
+  prompt([
+    {
+      type: 'list', 
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: ['Add department', 'Add role', 'Add employee', 'View departments', 'View roles', 'View employees', 'Update employee roles']
+    }
+  ])
+  .then(({ choice }) => {
+    console.log(choice)
+    // switch (choice) {
+    //   case 'Add department':
+    //     break
+    //   case 'Add role':
+    //     break
+    //   case 'Add employee':
+    //     break
+    //   case 'View departments':
+    //     break
+    //   case 'View roles':
+    //     break
+    //   case 'View employees':
+    //     break
+    //   case 'Update employee roles':
+    //     break
+    // }
+  })
+  .catch(err => console.log(err))
+}
+
+start()
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,2 +1,2 @@
-USE employee_db;
+
 
