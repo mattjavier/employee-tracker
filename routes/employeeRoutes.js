6aab0ed1af3b4cbb27099f11120ca2fb4e0b76bf
Matt Javier <matthewrjavier@gmail.com>
2020-08-24 11:15:31 -0700

    created necessary js files and sql files

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const db = require('../db')
+
+// GET all employees, roles, departments
+
+// POST new employees, roles, departments
+
+// PUT updates to employee roles
+
+// DELETE employees, roles, and departments
+
+// PUT updates to employee managers
+
+// GET employees by manager
